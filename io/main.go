e8ee2a27bc8b1a22db05ac787d02f19efce8542f
Sai628 <sai100628@gmail.com>
2017-02-03 22:07:53 +0800

    add io snippet

--- io/main.go
+++ io/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+    "os"
+    "fmt"
+)
+
+func writeToFile(name string, text string) (int, error) {
+    file, err := os.Create(name)
+    if err != nil {
+        return 0, err
+    }
+    defer file.Close()
+
+    return fmt.Fprintf(file, text)
+}
+
 func main() {
+    writeToFile("./result.test", "hello golang")
 }
